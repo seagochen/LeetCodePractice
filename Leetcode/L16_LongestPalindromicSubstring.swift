c66a3a256d9d8616b7b33ac5021e0373a45c7fb7
Orlando Chen <seagochen@hotmail.com>
2022-08-12 11:17:44 +0900

    Aug 12, 2022

---
+++ Leetcode/L16_LongestPalindromicSubstring.swift
@@ -0,0 +1,113 @@
+/**
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+
+
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+*/
+
+class Solution {
+
+    func isPalindrome(_ s: [Character], _ i: Int, _ j: Int) -> Bool {
+        var ii = i
+        var jj = j
+
+        while ii < jj {
+            if s[ii] != s[jj] {
+                return false
+            }
+            ii += 1
+            jj -= 1
+        }
+        return true
+    }
+
+    func longestPalindrome(_ s: String) -> String {
+        // convert string to array of characters
+        let chars = Array(s)
+
+        // if string is empty, return empty string
+        if chars.count == 0 {
+            return ""
+        }
+
+        // if string has only one character, return that character
+        if chars.count == 1 {
+            return s
+        }
+
+        // initialize variables
+        var longest = String(chars[0])
+
+        // search from first of string to last of string
+        for i in 0..<chars.count {
+            for j in (i..<chars.count).reversed() {
+                // if chars[i] == chars[j], then check if substring is palindrome
+                if chars[i] == chars[j] && j - i >= longest.count {
+                    if isPalindrome(chars, i, j) {
+                        longest = String(chars[i...j])
+                        // print(longest)
+                    }
+                }
+
+                if longest.count >= j - i {
+                    break
+                }
+            }
+        }
+
+        return longest
+    }
+}
+
+// class Solution {
+//     func longestPalindrome(_ s: String) -> String {
+//         if s.count < 2 {
+//             return s
+//         }
+        
+//         let chars = Array(s)
+//         var longest = ""
+        
+//         for i in 0..<chars.count {
+//             let odd = expand(chars, i, i)
+//             let even = expand(chars, i, i + 1)
+//             let max = odd.count > even.count ? odd : even
+//             if max.count > longest.count {
+//                 longest = max
+//             }
+//         }
+        
+//         return longest
+//     }
+
+//     func expand(_ chars: [Character], _ left: Int, _ right: Int) -> String {
+//         var left = left
+//         var right = right
+//         while left >= 0 && right < chars.count && chars[left] == chars[right] {
+//             left -= 1
+//             right += 1
+//         }
+//         return String(chars[left + 1..<right])
+//     }
+// }
+
+
+func test() {
+    let test_string1 = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+    let test_string2 = "cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
+
+    let solution = Solution()
+    print(solution.longestPalindrome(test_string1))
+    print(solution.longestPalindrome(test_string2))
+}
+
+test()
