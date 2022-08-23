6716e5d83bb6c6899d5b928a33a0d15bc1f4fb40
Orlando Chen <seagochen@hotmail.com>
2022-08-23 10:19:42 +0900

    Aug 23, 2022

---
+++ Leetcode/L26_PalindromeLinkedList.rs
@@ -0,0 +1,41 @@
+/**
+Given the head of a singly linked list, return true if it is a palindrome.
+
+Example 1:
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+Input: head = [1,2]
+Output: false
+*/
+
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+        let mut res = vec![];
+        while let Some(node) = head { 
+            res.push(node.val);
+            head = node.next
+        }
+        let mut tmp = res.clone();
+        tmp.reverse();
+        tmp == res;
+    }
+}
